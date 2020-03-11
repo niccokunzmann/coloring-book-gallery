8392e6916742e8fc40c98dcbb40c2401992fca58
Nicco Kunzmann <niccokunzmann@rambler.ru>
2020-03-11 01:39:30 +0100

    new desing of website

---
+++ js/applinks.js
@@ -0,0 +1,16 @@
+/*
+var APP_PROTOCOL = "coloring:";
+
+
+
+window.addEventListener("load", function() {
+  if (document.location.search.includes("app=true")) {
+    var applinks = document.getElementsByClassName("applink");
+    for (var i = 0; i < applinks.length; i++) {
+      var applink = applinks[i];
+      var newLink = APP_PROTOCOL + applink.href.substring(document.location.protocol.length);
+      applink.href = newLink;
+    }
+  }
+});
+*/
